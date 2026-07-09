c88638cfe7ab830fd4a47873c60bc6f67f9c5bab
Mohammed Yasin <mohammedyasinofficial@gmail.com>
2026-07-09 22:41:32 +0530

    added hashing(brute force way

---
+++ hashing.py
@@ -0,0 +1,9 @@
+n = [1,2,3,4,3,4,3,5,4,5,3,2,9,8,7,8,8]
+m = [10,111,1,12,2,24,34,3,4]
+
+for num in m:
+    count = 0
+    for x in n:
+        if num == x:
+            count += 1
+    print(count)
